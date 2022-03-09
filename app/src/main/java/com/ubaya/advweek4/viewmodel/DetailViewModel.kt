8fc560ef4f596bfa02ec18ba5b01000defe9ab57
Wirya2000 <s160419036@student.ubaya.ac.id>
2022-03-09 12:46:58 +0700

    init

---
+++ app/src/main/java/com/ubaya/advweek4/viewmodel/DetailViewModel.kt
@@ -0,0 +1,13 @@
+package com.ubaya.advweek4.viewmodel
+
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import com.ubaya.advweek4.model.Student
+
+class DetailViewModel : ViewModel() {
+    val studentLD = MutableLiveData<Student>()
+
+    fun fetch() {
+        studentLD.value = Student("16055","Nonie","1998/03/28","5718444778","http://dummyimage.com/75x100.jpg/cc0000/ffffff")
+    }
+}
